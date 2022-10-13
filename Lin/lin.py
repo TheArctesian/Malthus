371236cf4ce937969e6e5cc7135d3a0266e90551
TheArctesian <stephen.d.okita@gmail.com>
2022-10-13 21:07:24 +0800

    曹尼瑪

---
+++ Lin/lin.py
@@ -0,0 +1,19 @@
+import math
+from coalas import csvReader as c
+
+c.importCSV('population.csv')
+c.printHeaders()
+
+Pop = c.Population
+
+print(Pop)
+
+for i in range(len(Pop)):
+    temp = (float(c.Y25[i])-1)+math.log(126562371,2)
+    print(temp)
+
+for i in range(len(Pop)):
+    print(c.Y25[i])
+
+for i in range(len(Pop)):
+    print(math.log(float(Pop[i])),2)
