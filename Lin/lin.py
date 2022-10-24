2700613ae60f62bf8aef888555cd60ae84c1cf3b
TheArctesian <stephen.d.okita@gmail.com>
2022-10-24 08:01:20 +0800

    曹尼瑪

--- Lin/lin.py
+++ Lin/lin.py
@@ -8,12 +8,17 @@ Pop = c.Population
 
 print(Pop)
 
-for i in range(len(Pop)):
-    temp = (float(c.Y25[i])-1)+math.log(126562371,2)
-    print(temp)
+## for i in range(len(Pop)):
+##     temp = (float(c.Y25[i])-1)+math.log(126562371)
+##     print(temp)
+## 
+## for i in range(len(Pop)):
+##     print(c.Y25[i])
+## 
+## for i in range(len(Pop)):
+##     print(math.log(float(Pop[i])),2)
+## 
+# That was stupid 
+
 
 for i in range(len(Pop)):
-    print(c.Y25[i])
-
-for i in range(len(Pop)):
-    print(math.log(float(Pop[i])),2)
