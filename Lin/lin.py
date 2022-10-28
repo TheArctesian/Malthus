cb3d4f6f19ef282148733da6486c3b28f213b7e7
TheArctesian <stephen.d.okita@gmail.com>
2022-10-28 10:09:32 +0800

    曹尼瑪

--- Lin/lin.py
+++ Lin/lin.py
@@ -2,23 +2,14 @@ import math
 from coalas import csvReader as c
 
 c.importCSV('population.csv')
-c.printHeaders()
+c.addCol('log')
+#for i in range(len(Pop)):
+#    temp = (math.log(126562371.0)+(i-1)*math.log(t)
+#    print(temp)
+for i in range(len(c.Pop)):
+    # c.log.append(math.log(4184537.2)-(math.log(float(c.Pop[i]))/(i-1)-math.log(2)))
+    print((math.log(float(c.Pop[i]))-10))
+    c.log.append((math.log(float(c.Pop[i]))-10))
 
-Pop = c.Population
+c.writeCSV('lin.csv')
 
-print(Pop)
-
-## for i in range(len(Pop)):
-##     temp = (float(c.Y25[i])-1)+math.log(126562371)
-##     print(temp)
-## 
-## for i in range(len(Pop)):
-##     print(c.Y25[i])
-## 
-## for i in range(len(Pop)):
-##     print(math.log(float(Pop[i])),2)
-## 
-# That was stupid 
-
-
-for i in range(len(Pop)):
